8e99f677713060ff472a60e62ef2913ee5320f5e
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-11-04 10:38:30 +0700

    updated

---
+++ fucntion.go
@@ -0,0 +1,29 @@
+package gcfp
+
+import (
+    "fmt"
+    "net/http" 
+
+    "github.com/GoogleCloudPlatform/functions-framework-go/functions"
+    "github.com/PakArbi/pasetobackend"
+)
+
+func init() {
+    functions.HTTP("pakArbi", pasetoProPost)
+}
+
+func pasetoProPost(w http.ResponseWriter, r *http.Request) {
+    // Set CORS headers for the preflight request
+    if r.Method == http.MethodOptions {
+        w.Header().Set("Access-Control-Allow-Origin", "https://pakarbi.github.io")
+        w.Header().Set("Access-Control-Allow-Methods", "POST")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+        w.Header().Set("Access-Control-Max-Age", "3600")
+        w.WriteHeader(http.StatusNoContent)
+        return
+    }
+    // Set CORS headers for the main request.
+    w.Header().Set("Access-Control-Allow-Origin", "https://pakarbi.github.io")
+    fmt.Fprintf(w, pasetobackend.GCFPasetoTokenStr("privateKey", "MONGOVER", "pakArbi", "user", r))
+
+}
