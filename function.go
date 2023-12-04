97c83271972a4297568ddaae26c6d5f1c727fb0c
Muhammad Faisal A <sidiqfaisal30@gmail.com>
2023-12-04 11:16:32 +0700

    updates

--- function.go
+++ function.go
@@ -1,29 +1,51 @@
-package paseto
+// package gcp
+
+// import (
+// 	"fmt"
+// 	"net/http"
+
+// 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+// 	"github.com/PakArbi/backprofile"
+// )
+
+// func init() {
+// 	functions.HTTP("getData", getData)
+// }
+
+// func getData(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Access-Control-Allow-Origin", "https://pakarbi.github.io")
+// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization, Token")
+// 	w.Header().Set("Access-Control-Allow-Methods", "GET")
+// 	fmt.Fprintf(w, backprofile.GCHandlerFunc("MongoString", "PakArbi", "Profiles"))
+// }
+
+package gcf
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"github.com/PakArbi/backprofile/parkiran"
+	"net/http"
 
-    "github.com/GoogleCloudPlatform/functions-framework-go/functions"
-    "github.com/PakArbi/pasetobackend"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
 func init() {
-    functions.HTTP("loginData", backGCFkPost)
+	functions.HTTP("insertProfile", InsertdataProfileGCF)
 }
 
-func backGCFkPost(w http.ResponseWriter, r *http.Request) {
-    // Set CORS headers for the preflight request
-    if r.Method == http.MethodOptions {
-        w.Header().Set("Access-Control-Allow-Origin", "https://pakarbi.github.io")
-        w.Header().Set("Access-Control-Allow-Methods", "POST")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
-        w.Header().Set("Access-Control-Max-Age", "3600")
-        w.WriteHeader(http.StatusNoContent)
-        return
-    }
-    // Set CORS headers for the main request.
-    w.Header().Set("Access-Control-Allow-Origin", "https://pakarbi.github.io")
-    fmt.Fprintf(w, pasetobackend.GCFPostHandler("PASETOPRIVATEKEY","MONGOSTRING", "PakArbi", "user", r))
+func InsertdataProfileGCF(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://pakarbi.github.io")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization, Token, login")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "https://pakarbi.github.io")
+	w.Header().Add("Content-Type", "application/json")
+	fmt.Fprintf(w, parkiran.GCFPostParkiran("MONGOSTRING", "PakArbi", "Parkiran", r ))
 
 }
--- go.mod
+++ go.mod
@@ -1,42 +1,33 @@
-module github.com/projsonal/gcfp
+module github.com/Projsonal/gcfp
 
 go 1.20
 
 require (
 	github.com/GoogleCloudPlatform/functions-framework-go v1.8.0
-	github.com/PakArbi/pasetobackend v0.0.8
+	github.com/PakArbi/backprofile v0.1.2
 )
 
 require (
-	aidanwoods.dev/go-paseto v1.5.0 // indirect
-	aidanwoods.dev/go-result v0.1.0 // indirect
-	github.com/GilarYa/backendgis4 v0.0.2 // indirect
-	github.com/aiteung/atdb v0.1.3 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.14.0 // indirect
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
-	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/uuid v1.4.0 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.17.2 // indirect
-	github.com/microsoft/go-mssqldb v1.6.0 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
-	github.com/nugisOrange/backendgcf v0.0.3 // indirect
-	github.com/nugisOrange/petback v0.0.3 // indirect
-	github.com/whatsauth/watoken v0.1.3 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	go.mongodb.org/mongo-driver v1.13.0 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.26.0 // indirect
-	golang.org/x/crypto v0.15.0 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/image v0.14.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
